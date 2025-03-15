683c127280fa9a0dda335663a78d642b89a726b9
farhan1824 <farhanahmed1824@email.com>
2025-03-15 23:16:31 +0600

    added the sql files

---
+++ installation/sql/install.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `#__facebook_friends` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
---
+++ installation/sql/uninstall.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `#__facebook_friends`
